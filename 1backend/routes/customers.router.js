56d0e3510e1543b51802fe3e955bf5d016e2a99b
Jacob Larson <jacob.larson55112@gmail.com>
2025-09-23 18:04:51 -0500

    updated custoemrs route

--- 1backend/routes/customers.router.js
+++ 1backend/routes/customers.router.js
@@ -11,7 +11,6 @@ const TEMP_USER_ID = '52d9665a-3187-4b0c-8316-487784bf84a0'
 router.get('/', async (req, res) => {
     try {
         const userID = TEMP_USER_ID;
-        // first user will be the user used to interact with the web app durring v0.
         const { rows } = await pool.query(`
             SELECT * FROM customers
             WHERE user_id = $1
@@ -27,7 +26,6 @@ router.get('/', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
     try {
-        // first user will be the user used to interact with the web app durring v0.
         const { rows: [ user ] } = await pool.query(`
             SELECT * FROM customers
             WHERE id = $1
@@ -67,24 +65,25 @@ router.put('/:id', async(req, res) => {
         if(!name || !email || !phone) throw new Error('Missing Fields');
         const INSERT_SQL = [];
         const INSERT_PARAMS = [];
+        let idx = 2;
         if(name)
-            INSERT_SQL.push(`name = ${INSERT_SQL.length + 3}`);
+            INSERT_SQL.push(`name = ${idx++}`);
             INSERT_PARAMS.push(name);
         if(email)
-            INSERT_SQL.push(`email = ${INSERT_SQL.length + 3}`);
+            INSERT_SQL.push(`email = ${idx++}`);
             INSERT_PARAMS.push(email);
         if(phone)
-            INSERT_SQL.push(`phone = ${INSERT_SQL.length + 3}`);
+            INSERT_SQL.push(`phone = ${idx++}`);
             INSERT_PARAMS.push(phone);
 
         await pool.query(`
             UPDATE CUSTOMERS
-            SET ${INSERT_SQL}
+            SET ${INSERT_SQL.join(' ')}
             WHERE
                 customers.id = $1 AND user_id = $2
         `, [ customerID, userID, ...INSERT_PARAMS ]
         );
-        res.status(201).json({ message: 'User created successfully' });
+        res.status(200).json({ message: 'User updated successfully' });
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Something went wrong' });
