4c5bdd9e1810c31f42500e624faa74d88096339c
Jacob Larson <jacob.larson55112@gmail.com>
2025-09-23 17:50:35 -0500

    wip

--- 2frontend/src/state/zustand.js
+++ 2frontend/src/state/zustand.js
@@ -9,11 +9,9 @@ export const useStore = create((set) => ({
     error: null,
     setCustomers: (customers) => set({ customers }),
     fetchUser: async () => {
-        console.log(set)
         set({ isLoading: true, error: null })
         try {
             const res = await axios.get('/api/user')
-            console.log(res)
             set({ user: res.data, isLoading: false })
         } catch (err) {
             set({ error: err.message, isLoading: false })
