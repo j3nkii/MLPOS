e42183a5c7e491989822d159bcc1cde1af540a91
Jacob Larson <jacob.larson55112@gmail.com>
2025-10-16 18:10:20 -0500

    .

--- frontend/src/components/template/Layout.jsx
+++ frontend/src/components/template/Layout.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
+import React from 'react'
+import { Navigate, Outlet } from 'react-router-dom'
 import { useStateManager } from '@useStateManager'
 import { ModalManager, Navbar } from '@components'
 
 export const Layout = () => {
-    const { user } = useStateManager()
+    const { user } = useStateManager();
     if (!user)
-        return <Navigate to="/login" replace />;
+        return <Navigate to="/login" replace />
     else return (
         <div className='min-h-screen bg-gray-400'>
             <Navbar />
@@ -17,4 +17,4 @@ export const Layout = () => {
         </div>
         
     );
-};
+}
