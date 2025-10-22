fcb35ba3027191b62dd6ae994b8a2d6980d92049
Jacob Larson <jacob.larson55112@gmail.com>
2025-10-21 19:30:57 -0500

    init invoice form....

---
+++ frontend/src/components/modals/InvoiceFormModal.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect } from 'react';
+import { Modal, ModalHeader, ModalBody, ModalFooter } from '@components';
+import { useStateManager } from '@useStateManager';
+import { Button, Input } from '@components';
+
+export const InvoiceFormModal = ({ update }) => {
+    const { customerForm } = useStateManager();
+    const { closeModal, setCustomerForm, submitNewCustomer, prepopulateCustomerForm, resetCustomerForm, updateCustomer } = useStateManager();
+
+    useEffect(() => {
+        if(update)
+            prepopulateCustomerForm();
+    }, [])
+
+    const handleConfirm = async () => {
+        update ? updateCustomer(customerForm.id) : submitNewCustomer();
+    };
+
+    const handleChange = (evt) => {
+        const { target: { name, value }} = evt;
+        setCustomerForm({ name, value });
+    }
+
+    return (
+        <Modal onClose={closeModal}>
+            <ModalHeader title={'Confirm'} onClose={closeModal} />
+            <ModalBody>
+                <form className="p-6">
+                    <Input onChange={handleChange} value={customerForm.name} label={'Name'} name={'name'} />
+                    <Input onChange={handleChange} value={customerForm.email} label={'Email'} name={'email'} />
+                    <Input onChange={handleChange} value={customerForm.phone} label={'Phone'} name={'phone'} />
+                </form>
+            </ModalBody>
+
+            <ModalFooter>
+                <Button onClick={handleConfirm}>Create</Button>
+                <Button onClick={closeModal}>Cancel</Button>
+            </ModalFooter>
+        </Modal>
+    );
+}
