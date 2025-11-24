b0e499ec8b62c8ad5dd0a52043b6576ae83e9037
j3nkii <jacob.larson55112@gmail.com>
2025-11-24 17:57:23 -0600

    deploy

--- backend/modules/pool.js
+++ backend/modules/pool.js
@@ -1,18 +1,23 @@
 const pg = require('pg');
 require('dotenv').config(); 
-
-const config = {
+const configProd = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+};
+const configDev = {
     host: 'localhost',
     port: 5432,
     database: process.env.DB_NAME,
     max: 10,
     idleTimeoutMillis: 30000,
 };
+const config = process.env.NODE_ENV === "production" ? configProd : configDev;
 const pool = new pg.Pool(config);
 pool.on('error', (err) => {
     console.error('Unexpected error on idle client', err);
     process.exit(-1);
 });
-
-
 module.exports = pool;
