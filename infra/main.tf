2fd8cb244168ba7936cdfbae79e6430fcc4ac14c
j3nkii <jacob.larson55112@gmail.com>
2025-11-24 18:40:42 -0600

    tf apply

--- infra/main.tf
+++ infra/main.tf
@@ -252,6 +252,13 @@ resource "aws_security_group" "rds_sg" {
     protocol    = "tcp"
     security_groups = [aws_security_group.lambda_sg.id]
   }
+#   TODO REMOVE IN PRODUCTION
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
