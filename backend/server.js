f1d39186385baea75b28a95c580312e1b91a30fa
j3nkii <jacob.larson55112@gmail.com>
2025-12-04 16:13:30 -0600

    added in env stuff, changes package script to local for local dev

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const app = require('./app');
+require('dotenv').config();
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT} :: ENV ${process.env.ENVIRONMENT}`);
+
 });
