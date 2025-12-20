e8eefbda525c0dea1c68be4d2618616f22721ca2
j3nkii <jacob.larson55112@gmail.com>
2025-12-19 19:03:06 -0600

    remove app logger for in/out reqs.

--- backend/app.js
+++ backend/app.js
@@ -7,22 +7,6 @@ const authMiddleware = require('./middleware/auth.middleware');
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-  if(process.env.NODE_ENV === 'production'){
-    console.log('=== INCOMING REQUEST ===');
-    console.log('Path:', req.path);
-    console.log('Query params:', req.query);
-    console.log('Body:', req.body);
-    const originalSend = res.send;
-    res.send = function(data) {
-      console.log('=== OUTGOING RESPONSE ===');
-      console.log('Status:', res.statusCode);
-      console.log('Response body:', data);
-      originalSend.call(this, data);
-    };
-  }
-  next();
-});
 
 
 // const SAMPLE = require('./routers/SAMPLE.router');
