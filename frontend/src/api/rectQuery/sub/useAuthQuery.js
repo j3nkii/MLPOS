886c80484069a215327fb65ead88aac500b34c3b
j3nkii <jacob.larson55112@gmail.com>
2026-03-23 14:48:27 -0500

    wip

---
+++ frontend/src/api/rectQuery/sub/useAuthQuery.js
@@ -0,0 +1,37 @@
+import { useMutation } from '@tanstack/react-query';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '@useZustand';
+import { authService } from '@services';
+
+export const useAuthActions = () => {
+    const navigate = useNavigate();
+    const { setPageView, loginForm } = useAuth();
+
+    const postConfirmation = useMutation({
+        mutationFn: (body) => authService.createConfirm(body),
+        onError: (error) => console.error(error),
+    })
+
+    const fetchUser = useMutation({
+        mutationFn: () => authService.readAuth(loginForm),
+        onSuccess: (res) => {
+            sessionStorage.setItem('accessToken', res.data.tokens.accessToken);
+            sessionStorage.setItem('refreshToken', res.data.tokens.refreshToken);
+            sessionStorage.setItem('idToken', res.data.tokens.idToken);
+            navigate('/customers');
+        },
+        onError: (error) => console.error(error),
+    })
+
+    const createUser = useMutation({
+        mutationFn: (body) => authService.createAuth(body),
+        onSuccess: () => setPageView('confirm'),
+        onError: (error) => console.error(error),
+    })
+
+    return {
+        postConfirmation,
+        fetchUser,
+        createUser,
+    }
+}
