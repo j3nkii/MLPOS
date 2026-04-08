33e23450a841e4fee1ca5ccdc5aa5fe12ef0f9ab
j3nkii <jacob.larson55112@gmail.com>
2026-04-08 15:52:41 -0500

    seperatign user and auth state handlers

--- frontend/src/api/reactQuery/index.js
+++ frontend/src/api/reactQuery/index.js
@@ -1,3 +1,4 @@
 export { useCustomerQuery } from './sub/useCustomerQuery';
 export { useAuthQuery } from './sub/useAuthQuery';
 export { useInvoiceQuery } from './sub/useInvoiceQuery';
+export { useUserQuery } from './sub/useUserQuery';
