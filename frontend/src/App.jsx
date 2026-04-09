d3995b046d22105c058a2c2c73270913939fa044
j3nkii <jacob.larson55112@gmail.com>
2026-04-09 14:17:20 -0500

    cu

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,16 @@
 import 'react';
-import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { createBrowserRouter, createHashRouter, RouterProvider } from 'react-router-dom';
 import { routerConfig } from './config/routerConfig';
+import { queryClient } from '@query';
 import './config/axiosConfig';
-
-
-const client = new QueryClient({
-    defaultOptions: {
-        queries: {
-            staleTime: 1000 * 60 * 5, // 5 minutes
-        }
-    }
-});
 
 
 function App() {
   const router = createHashRouter(routerConfig);
   return (
     <div>
-      <QueryClientProvider client={client} >
+      <QueryClientProvider client={queryClient} >
         <RouterProvider router={router} />
       </QueryClientProvider>
     </div>
--- frontend/src/api/reactQuery/index.js
+++ frontend/src/api/reactQuery/index.js
@@ -2,3 +2,13 @@ export { useCustomerQuery } from './sub/useCustomerQuery';
 export { useAuthQuery } from './sub/useAuthQuery';
 export { useInvoiceQuery } from './sub/useInvoiceQuery';
 export { useUserQuery } from './sub/useUserQuery';
+
+
+import { QueryClient } from '@tanstack/react-query';
+export const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            staleTime: 1000 * 60 * 5, // 5 minutes
+        }
+    }
+});
