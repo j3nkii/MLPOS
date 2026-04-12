b9f5e48eeb3e4c3a24f483977e7fd6acba7e5a04
j3nkii <jacob.larson55112@gmail.com>
2026-04-12 04:46:16 -0500

    update schema.

--- database/schema.sql
+++ database/schema.sql
@@ -1,13 +1,36 @@
--- system users (the businesses / staff)
+-- accounts / tennants , used to umbrella users .
+DROP TABLE IF EXISTS accounts CASCADE;
+CREATE TABLE account (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+);
+
+
+
+DROP TABLE IF EXISTS auth_confirmation;
+CREATE TABLE auth_confirmation (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID NOT NULL REFERENCES users(id),
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+)
+
+
+
+-- account users
 DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    account_id UUID NOT NULL REFERENCES accounts(id),
+    role VARCHAR(320) NOT NULL VARCHAR(50),
     username VARCHAR(320) UNIQUE NOT NULL,
     email VARCHAR(320) UNIQUE NOT NULL,
     is_deleted BOOLEAN DEFAULT FALSE,
     created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
+
+
 
 -- customers (end clients of the businesses)
 DROP TABLE IF EXISTS customers CASCADE;
@@ -22,18 +45,22 @@ CREATE TABLE customers (
     updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
 
--- invoices (example POS feature)
+
+
+-- invoices, to contain: details(line items), payments.
 DROP TABLE IF EXISTS invoices CASCADE;
 CREATE TABLE invoices (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    user_id UUID NOT NULL REFERENCES users(id),       -- business owner
-    customer_id UUID NOT NULL REFERENCES customers(id), -- end client
+    user_id UUID NOT NULL REFERENCES users(id),
+    customer_id UUID NOT NULL REFERENCES customers(id),
     status VARCHAR(50) DEFAULT 'pending', -- pending, paid, cancelled, overdue
     date_sent TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
     is_deleted BOOLEAN DEFAULT FALSE,
     created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
+
+
 
 DROP TABLE IF EXISTS invoices_details CASCADE;
 CREATE TABLE invoices_details (
@@ -47,6 +74,8 @@ CREATE TABLE invoices_details (
     updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
 
+
+
 DROP TABLE IF EXISTS payments CASCADE;
 CREATE TABLE payments (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
@@ -59,9 +88,19 @@ CREATE TABLE payments (
     updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
 
--- DROP TABLE IF EXISTS auth_confirmation;
--- CREATE TABLE auth_confirmation (
---     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
---     user_id UUID NOT NULL REFERENCES users(id),
---     created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
--- )
+
+
+DROP TABLE IF EXISTS product CASCADE;
+CREATE TABLE inventory (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+
+    price INTEGER NOT NULL,
+    unit VARCHAR(50) NOT NULL -- single, gram, oz, pound, ..ect,
+    wholesale_price INTEGER, 
+    internal_sku VARCHAR(50) NOT NULL, -- maybe define default sku system for mlpos inventory.
+    external_sku VARCHAR(50) NOT NULL,
+
+    is_deleted BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+);
