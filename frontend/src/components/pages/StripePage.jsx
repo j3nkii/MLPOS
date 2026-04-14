098e90b8a3d2fc85c381f3c4ae5a48f55da2a028
j3nkii <jacob.larson55112@gmail.com>
2026-04-14 18:56:13 -0500

    stripe kinda working on front end ???

--- frontend/src/components/pages/StripePage.jsx
+++ frontend/src/components/pages/StripePage.jsx
@@ -1,16 +1,37 @@
-import 'react';
+import { useState } from 'react';
 import { Table, Button } from '@components';
 import { useInvoiceQuery } from '@query';
 import { stripeService } from '@services';
 import { ConnectAccountOnboarding, ConnectComponentsProvider } from "@stripe/react-connect-js";
+import { loadConnectAndInitialize } from '@stripe/connect-js';
+
 
 
 
 export const StripePage = () => {
-    const handleOnboard = async () => {
-        const response = await stripeService.createAccount();
-        console.log(response.data.url)
-    }
+    const [errorMessage, setErrorMessage] = useState('');
+    const [stripeConnectInstance] = useState(() => {
+        const fetchClientSecret = async () => {
+            try {
+                const response = await stripeService.createAccount(null);
+                return response.data.client_secret;
+            } catch (error) {
+                console.error('An error occurred: ', error);
+                setErrorMessage(error)
+                return undefined;
+            }
+        }
+        return loadConnectAndInitialize({
+                // This is your test publishable API key.
+                publishableKey: "pk_test_51TMEAFDbglfMtuu11wAs5SCkfrG1dLPJXmynHXNaybPvTCwfVCPX2x8RCpRg9Eo5jy5U8bHbOM6C55o42C4BOh9200qEzCbyae",
+                fetchClientSecret: fetchClientSecret,
+            })
+        });
+    // const handleOnboard = async () => {
+    //     const response = await stripeService.createAccount(null);
+    //     console.log(response.data);
+    //     setSessionSecret(response.data.client_secret)
+    // }
 
     //   const handleStartOnboarding = async () => {
     //     try {
@@ -36,7 +57,7 @@ export const StripePage = () => {
         <div className='max-w-170 bg-white'>
             <h1 className='p-10 pt-10 text-4xl font-extrabold no-wrap'>Invoices:</h1>
             <Button onClick={handleOnboard} >On BOARD SOMEONE</Button>
-            {/* <ConnectComponentsProvider connectInstance={{}}>
+            <ConnectComponentsProvider connectInstance={stripeConnectInstance}>
             <ConnectAccountOnboarding
                 onExit={() => {
                     console.log("The account has exited onboarding");
@@ -56,7 +77,7 @@ export const StripePage = () => {
                 //   console.log(`User entered: ${stepChange.step}`);
                 // }}
                 />
-            </ConnectComponentsProvider> */}
+            </ConnectComponentsProvider>
 
         </div>
     );
