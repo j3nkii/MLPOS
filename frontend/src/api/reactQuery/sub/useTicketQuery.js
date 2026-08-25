56c614d669cb39020b596240566ce92c68cb497a
j3nkii <jacob.larson55112@gmail.com>
2026-08-25 09:01:17 -0500

    RQ tickets hooked into toast tabs

--- frontend/src/api/reactQuery/sub/useTicketQuery.js
+++ frontend/src/api/reactQuery/sub/useTicketQuery.js
@@ -1,7 +1,7 @@
 import 'react';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { ticketService } from '@services';
-import { useModalZussy} from '@zussy';
+import { useModalZussy, useToastZussy } from '@zussy';
 import { useNavigate } from 'react-router-dom';
 
 
@@ -9,13 +9,16 @@ import { useNavigate } from 'react-router-dom';
 export const useTicketQuery = () => {
     const navigate = useNavigate();
     const queryClient = useQueryClient();
+    const { addError, addSuccess } = useToastZussy();
     const { closeModal } = useModalZussy();
 
     const _refreshTickets = async() => {
         await queryClient.fetchQuery({
             queryKey: ['allTickets'],
             queryFn: ticketService.readAllTickets,
-            onError: (error) => console.error(error)
+            onError: (error) => {
+                console.error(error);
+            }
         });
     }
 
@@ -25,23 +28,31 @@ export const useTicketQuery = () => {
             navigate(`/tickets/${result.data.data.ticketID}`)
             await _refreshTickets()
             queryClient.invalidateQueries({ queryKey: ['allTickets'] });
+            addSuccess('Ticket created.');
             closeModal();
         },
-        onError: (error) => console.error(error),
+        onError: (error) => {
+            addError('Ticket creation failed.');
+            console.error(error);
+        },
     });
 
     // const readTicket = useQuery({
     //     queryKey: ['invocie', ticketID],
     //     queryFn: () => ticketService.readTicket(ticketID),
     //     onSuccess: (res) => setSelectedTicket(res.data),
-    //     onError: (error) => console.error(error),
+    //     onError: (error) => {
+        //     console.error(error);
+        // },
     //     enabled: !!ticketID,
     // })
 
     const readAllTickets = useQuery({
         queryKey: ['allTickets'],
         queryFn: ticketService.readAllTickets,
-        onError: (error) => console.error(error),
+        onError: (error) => {
+            console.error(error);
+        },
     });
 
     const updateTicket = useMutation({
@@ -49,19 +60,27 @@ export const useTicketQuery = () => {
         onSuccess: async () => {
             await _refreshTickets()
             queryClient.invalidateQueries({ queryKey: ['allTickets'] });
+            addSuccess('Ticket updated.');
             closeModal();
         },
-        onError: (error) => console.error(error),
+        onError: (error) => {
+            addError('Ticket update failed.');
+            console.error(error);
+        },
     });
 
     const deleteTicket = useMutation({
         mutationFn: ticketService.deleteTicket,
         onSuccess: () => {
             queryClient.invalidateQueries({ queryKey: ['allTickets'] });
+            addSuccess('Ticket deleted.');
             closeModal();
             navigate(`/tickets`)
         },
-        onError: (error) => console.error(error),
+        onError: (error) => {
+            addError('Ticket deletion failed.');
+            console.error(error);
+        },
     });
 
 
@@ -71,19 +90,27 @@ export const useTicketQuery = () => {
         onSuccess: async () => {
             await _refreshTickets()
             queryClient.invalidateQueries({ queryKey: ['allTickets'] });
+            addSuccess('Ticket Item updated.');
             closeModal();
         },
-        onError: (error) => console.error(error),
+        onError: (error) => {
+            addError('Ticket Item update failed.');
+            console.error(error);
+        },
     });
 
     const createTicketItem = useMutation({
         mutationFn: ticketService.createTicketItem,
         onSuccess: async () => {
             // not super sure why this had to be done this way in order to work. invalidate was not refreshing data. 
-            await _refreshTickets()
+            await _refreshTickets();
             queryClient.invalidateQueries({ queryKey: ['allTickets'] });
+            addSuccess('Ticket Item created.');
         },
-        onError: (error) => console.error(error),
+        onError: (error) => {
+            addError('Ticket Item creation failed.');
+            console.error(error);
+        },
     });
 
     const deleteTicketItem = useMutation({
@@ -92,9 +119,13 @@ export const useTicketQuery = () => {
             // not super sure why this had to be done this way in order to work. invalidate was not refreshing data. 
             await _refreshTickets()
             queryClient.invalidateQueries({ queryKey: ['allTickets'] });
+            addSuccess('Ticket Item deleted.');
             closeModal();
         },
-        onError: (error) => console.error(error),
+        onError: (error) => {
+            addError('Ticket Item deletion failed.');
+            console.error(error);
+        },
     });
 
     const createTicketSend = useMutation({
@@ -103,9 +134,13 @@ export const useTicketQuery = () => {
             // not super sure why this had to be done this way in order to work. invalidate was not refreshing data. 
             await _refreshTickets()
             queryClient.invalidateQueries({ queryKey: ['allTickets'] });
+            addSuccess('Ticket sent successfully.');
             closeModal();
         },
-        onError: (error) => console.error(error),
+        onError: (error) => {
+            addError('Ticket send failed.');
+            console.error(error);
+        },
     });
 
     return {
