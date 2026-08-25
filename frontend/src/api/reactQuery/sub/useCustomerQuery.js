c710bd734c38c4a73c7d496bf205db78e9040e1f
j3nkii <jacob.larson55112@gmail.com>
2026-08-25 08:51:12 -0500

    RQcustomers hooked into toasts

--- frontend/src/api/reactQuery/sub/useCustomerQuery.js
+++ frontend/src/api/reactQuery/sub/useCustomerQuery.js
@@ -1,6 +1,6 @@
 import 'react';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
-import { useModalZussy} from '@zussy';
+import { useModalZussy, useToastZussy } from '@zussy';
 
 
 import { customerService } from '@services';
@@ -9,14 +9,19 @@ import { customerService } from '@services';
 export const useCustomerQuery = () => {
     const queryClient = useQueryClient();
     const { closeModal } = useModalZussy();
+    const { addError, addSuccess } = useToastZussy();
 
     const createCustomer = useMutation({
         mutationFn: (body) => customerService.createCustomer(body),
         onSuccess: () => {
             queryClient.invalidateQueries({ queryKey: ['allCustomers'] });
+            addSuccess('Customer created.');
             closeModal();
         },
-        onError: (error) => console.error(error),
+        onError: (error) => {
+            addError('Customer creation failed.');
+            console.error(error);
+        },
     })
 
     // const readCustomer = useQuery({
@@ -37,18 +42,26 @@ export const useCustomerQuery = () => {
         mutationFn: (body) => customerService.updateCustomer(body),
         onSuccess: () => {
             queryClient.invalidateQueries({ queryKey: ['allCustomers'] });
+            addSuccess('Customer updated.');
             closeModal();
         },
-        onError: (error) => console.error(error),
+        onError: (error) => {
+            addError('Customer update failed.');
+            console.error(error);
+        },
     })
 
     const deleteCustomer = useMutation({
         mutationFn: (customerID) => customerService.deleteCustomer(customerID),
         onSuccess: () => {
             queryClient.invalidateQueries({ queryKey: ['allCustomers'] });
+            addSuccess('Customer deleted.');
             closeModal();
         },
-        onError: (error) => console.error(error),
+        onError: (error) => {
+            addError('Customer deletion failed.');
+            console.error(error)
+        },
     })
 
     return {
