fd2b52bd8f4460abd5dbc977a04ee95c4f8c0e8f
j3nkii <jacob.larson55112@gmail.com>
2026-09-07 10:38:49 -0500

    ope

--- frontend/src/api/services/sub/ticket.js
+++ frontend/src/api/services/sub/ticket.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export const ticketService = {
     readAllTickets: () => axios.get(`/api/ticket`).then(res => res.data),
     createTicket: ({ body }) => axios.post('/api/ticket', body).then(res => res.data),
-    readTicket: ({ ticketID }) => axios.get(`/api/ticket/${ticketID}`).then(res => res.data).then(res => res.data),
+    readTicket: ({ ticketID }) => axios.get(`/api/ticket/${ticketID}`).then(res => res.data),
     updateTicket: ({ ticketID, body }) => axios.put(`/api/ticket/${ticketID}`, body).then(res => res.data),
     deleteTicket: ({ ticketID }) =>  axios.delete(`/api/ticket/${ticketID}`).then(res => res.data),
     createTicketSend: ({ ticketID }) =>  axios.post(`/api/ticket/send/${ticketID}`).then(res => res.data),
