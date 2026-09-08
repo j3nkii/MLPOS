18e61d050eab83f67169f1e72b1552033ff4295f
j3nkii <jacob.larson55112@gmail.com>
2026-09-08 09:19:05 -0500

    schema file cleanup

--- database/schema.sql
+++ database/schema.sql
@@ -1,13 +1,49 @@
+DROP TYPE IF EXISTS ticket_invoice_status CASCADE;
+CREATE TYPE ticket_invoice_status AS ENUM (
+    'quote',
+    'sent',
+    'partially_paid',
+    'overdue',
+    'paid',
+    'cancelled'
+);
 
+DROP TYPE IF EXISTS ticket_order_status CASCADE;
+CREATE TYPE ticket_order_status AS ENUM (
+    'waiting',
+    'in_progress',
+    'fufilled',
+    'rework',
+    'cancelled'
+);
+
+DROP TYPE IF EXISTS product_type CASCADE;
+CREATE TYPE product_type AS ENUM (
+    'service',
+    'physical'
+);
+
+DROP TYPE IF EXISTS payment_method_type CASCADE;
+CREATE TYPE payment_method_type AS ENUM (
+    'cash',
+    'check',
+    'venmo',
+    'cash app',
+    'square',
+    'paypal',
+    'zelle',
+    'bitcoin',
+    'stripe'
+);
 
 -- accounts / tenants — umbrella for users and business data
 DROP TABLE IF EXISTS accounts CASCADE;
 CREATE TABLE accounts (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    name VARCHAR(256) NOT NULL,
-    slug VARCHAR(63) UNIQUE,
-    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+    id          UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name        VARCHAR(256) NOT NULL,
+    slug        VARCHAR(63) UNIQUE,
+    created_at  TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    updated_at  TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
 
 
@@ -29,119 +65,81 @@ CREATE TABLE accounts_stripe (
 
 DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    account_id UUID NOT NULL REFERENCES accounts(id),
-    role VARCHAR(320) NOT NULL DEFAULT 'primary',
-    username VARCHAR(320) UNIQUE NOT NULL,
-    email VARCHAR(320) UNIQUE NOT NULL,
-    is_deleted BOOLEAN DEFAULT FALSE,
-    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+    id          UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    account_id  UUID NOT NULL REFERENCES accounts(id),
+    role        VARCHAR(320) NOT NULL DEFAULT 'primary',
+    username    VARCHAR(320) UNIQUE NOT NULL,
+    email       VARCHAR(320) UNIQUE NOT NULL,
+    is_deleted  BOOLEAN DEFAULT FALSE,
+    created_at  TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    updated_at  TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
 
 
 
 DROP TABLE IF EXISTS customers CASCADE;
 CREATE TABLE customers (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    account_id UUID NOT NULL REFERENCES accounts(id),
-    user_id UUID NOT NULL REFERENCES users(id),
-    name VARCHAR(255),
-    email VARCHAR(320),
-    phone VARCHAR(50),
-    is_deleted BOOLEAN DEFAULT FALSE,
-    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+    id          UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    account_id  UUID NOT NULL REFERENCES accounts(id),
+    user_id     UUID NOT NULL REFERENCES users(id),
+    name        VARCHAR(255),
+    email       VARCHAR(320),
+    phone       VARCHAR(50),
+    is_deleted  BOOLEAN DEFAULT FALSE,
+    created_at  TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    updated_at  TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
 
 
-
-DROP TYPE IF EXISTS ticket_invoice_status CASCADE;
-CREATE TYPE ticket_invoice_status AS ENUM (
-    'quote',
-    'sent',
-    'partially_paid',
-    'overdue',
-    'paid',
-    'cancelled'
-);
-
-DROP TYPE IF EXISTS ticket_order_status CASCADE;
-CREATE TYPE ticket_order_status AS ENUM (
-    'waiting',
-    'in_progress',
-    'fufilled',
-    'rework',
-    'cancelled'
-);
-
-
-DROP TYPE IF EXISTS product_type CASCADE;
-CREATE TYPE product_type AS ENUM (
-    'service',
-    'physical'
-);
 DROP TABLE IF EXISTS products CASCADE;
 CREATE TABLE products (
+    id               UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    account_id       UUID NOT NULL REFERENCES accounts(id),
+    name             VARCHAR(255) NOT NULL,
+    description      VARCHAR(255),
+    product_type     product_type,
+    price            INTEGER NOT NULL,
+    wholesale_price  INTEGER,
+    internal_sku     VARCHAR(50),
+    external_sku     VARCHAR(50),
+    api_available    BOOLEAN DEFAULT false NOT NULL,
+    is_bookable      BOOLEAN DEFAULT false NOT NULL,
+    is_shippable     BOOLEAN DEFAULT false NOT NULL,
+    is_deleted       BOOLEAN DEFAULT FALSE,
+    created_at       TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    updated_at       TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+);
+
+
+DROP TABLE IF EXISTS tickets CASCADE;
+CREATE TABLE tickets (
     id              UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     account_id      UUID NOT NULL REFERENCES accounts(id),
-    name            VARCHAR(255) NOT NULL,
-    description     VARCHAR(255),
-    product_type    product_type,
-    price           INTEGER NOT NULL,
-    wholesale_price INTEGER,
-    internal_sku    VARCHAR(50),
-    external_sku    VARCHAR(50),
-    api_available   BOOLEAN DEFAULT false NOT NULL,
-    is_bookable     BOOLEAN DEFAULT false NOT NULL,
-    is_shippable    BOOLEAN DEFAULT false NOT NULL,
+    user_id         UUID NOT NULL REFERENCES users(id),
+    customer_id     UUID REFERENCES customers(id),
+    invoice_status  ticket_invoice_status DEFAULT 'quote',
+    order_status    ticket_order_status DEFAULT 'waiting',
     is_deleted      BOOLEAN DEFAULT FALSE,
     created_at      TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
     updated_at      TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
 
 
-DROP TABLE IF EXISTS tickets CASCADE;
-CREATE TABLE tickets (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    account_id UUID NOT NULL REFERENCES accounts(id),
-    user_id UUID NOT NULL REFERENCES users(id),
-    customer_id UUID REFERENCES customers(id),
-    invoice_status ticket_invoice_status DEFAULT 'quote',
-    order_status ticket_order_status DEFAULT 'waiting',
-    is_deleted BOOLEAN DEFAULT FALSE,
-    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
-);
-
-
 
 DROP TABLE IF EXISTS ticket_items CASCADE;
 CREATE TABLE ticket_items (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    ticket_id UUID NOT NULL REFERENCES tickets(id),
-    product_id UUID REFERENCES products(id),
-    name VARCHAR(257),
-    quantity INTEGER,
-    price INTEGER,
-    is_deleted BOOLEAN DEFAULT FALSE,
-    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+    id          UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    ticket_id   UUID NOT NULL REFERENCES tickets(id),
+    product_id  UUID REFERENCES products(id),
+    name        VARCHAR(257),
+    quantity    INTEGER,
+    price       INTEGER,
+    is_deleted  BOOLEAN DEFAULT FALSE,
+    created_at  TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    updated_at  TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
 
 
-DROP TYPE IF EXISTS payment_method_type CASCADE;
-CREATE TYPE payment_method_type AS ENUM (
-    'cash',
-    'check',
-    'venmo',
-    'cash app',
-    'square',
-    'paypal',
-    'zelle',
-    'bitcoin',
-    'stripe'
-);
 DROP TABLE IF EXISTS payments CASCADE;
 CREATE TABLE payments (
     id          UUID PRIMARY KEY DEFAULT gen_random_uuid(),
@@ -158,25 +156,25 @@ CREATE TABLE payments (
 
 DROP TABLE IF EXISTS sent_payments CASCADE;
 CREATE TABLE sent_payments (
-    id                      UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    ticket_id               UUID NOT NULL REFERENCES tickets(id),
-    is_deleted BOOLEAN      DEFAULT FALSE,
-    created_at TIMESTAMPTZ  DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMPTZ  DEFAULT CURRENT_TIMESTAMP
+    id          UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    ticket_id   UUID NOT NULL REFERENCES tickets(id),
+    is_deleted  BOOLEAN DEFAULT FALSE,
+    created_at  TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    updated_at  TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
 
 
 
 DROP TABLE IF EXISTS appointments CASCADE;
 CREATE TABLE appointments (
-    id                      UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    ticket_id               UUID REFERENCES tickets(id),
-    customer_id             UUID REFERENCES customers(id),
-    book_start              TIMESTAMPTZ NOT NULL,
-    book_end                TIMESTAMPTZ NOT NULL,
-    is_deleted BOOLEAN      DEFAULT FALSE,
-    created_at TIMESTAMPTZ  DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMPTZ  DEFAULT CURRENT_TIMESTAMP
+    id           UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    ticket_id    UUID REFERENCES tickets(id),
+    customer_id  UUID REFERENCES customers(id),
+    book_start   TIMESTAMPTZ NOT NULL,
+    book_end     TIMESTAMPTZ NOT NULL,
+    is_deleted   BOOLEAN DEFAULT FALSE,
+    created_at   TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    updated_at   TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
 
 
